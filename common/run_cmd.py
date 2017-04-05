be4e5bdd8f863d6d604efde44c31d0e60d49010d
kHRYSTAL <723526676@qq.com>
2017-04-05 10:35:40 +0800

    update

--- common/run_cmd.py
+++ common/run_cmd.py
@@ -79,5 +79,82 @@ def do_ssh(ip, username, password, commands, pipe_name="/dev/null"):
     # pprint(str_buff.getValue)
     return str_buff.getvalue()
 
+
+def do_telnet(ip, username, password, commands, pipe_name="/dev/null"):
+    if not isinstance(commands, list):
+        logger.error("type of commands parameter should be a list")
+        return
+        # try to open log file
+    try:
+        log_f = RecordFile(filename=pipe_name, when='m', interval=1, backupCount=48)
+    except Exception, e:
+        logger.warn(ip, "Continue with log_file not present", e)
+        log_f = None
+        # open String IO stream, for improve the processing speed,
+        # because write to file then read will cause double effort
+    str_buff = StringIO()
+
+    try:
+        telnet = telnetlib.Telnet(ip)
+        telnet.read_until("Username:")
+        telnet.write(username.encode('ascii', 'ignore') + "\n")
+        telnet.read_until("Password:")
+        telnet.write(password.encode('ascii', 'ignore') + "\n")
+
+        for cmd in commands:
+            cmd_separator = "=" * 10 + " " + ip + " == " + cmd + " " + "-" * 10 + "\n"
+            cmd_starter = "===START===\n"
+            cmd_stopper = "---STOP---\n"
+            logger.debug("Running command %s" % cmd.encode('ascii', 'ignore'))
+            try:
+                telnet.write(cmd.encode('ascii', 'ignore') + "\n")
+                _, _, stdout = telnet.expect(["<.*?>"], timeout=5)
+
+                # WORKAROUND: fix later
+                while not cmd.encode('ascii', 'ignore') in stdout:
+                    _, _, stdout = telnet.expect(["<.*?>"], timeout=5)
+
+                if "screen-length" in cmd:
+                    continue
+
+                if log_f:
+                    log_f.write(cmd_separator)
+                    log_f.write(cmd_starter)
+                str_buff.write(cmd_separator)
+                str_buff.write(cmd_starter)
+
+                if log_f:
+                    log_f.write(stdout)
+                str_buff.write(stdout)
+
+                if log_f:
+                    log_f.write(cmd_stopper)
+                str_buff.write(cmd_stopper)
+            except Exception, e:
+                logger.error(e)
+
+        logger.info('%s\tOK' % ip)
+        telnet.close()
+        log_f.flush()
+        log_f.close()
+    except Exception, e:
+        logger.error('%s\tError: %s' % (ip, e))
+
+    # pprint(str_buff.getvalue())
+    return str_buff.getvalue()
+
+
+def run_cmd(ip, username, password, cmd, parse_func, conn_method='ssh', log_dir='.'):
+    logger.info("Checking %s ..." % ip)
+    str_buffer = ''
+    if conn_method == 'ssh':
+        pass
+    elif conn_method == 'telnet':
+        pass
+    else:
+        raise Exception("Unknow connect method")
+    logger.debug("Parsing string buffer to json %s" % ip)
+    return parse_func(str_buffer)
+
 if __name__ == '__main__':
     pass
